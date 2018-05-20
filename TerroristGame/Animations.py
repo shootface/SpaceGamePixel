3a853386a2fee2b57ab87eaa0a72bb77b17c2545
David Aparicio <davidalejandro1223@hotmail.com>
2018-05-19 22:43:01 -0500

    clase animations convertida a clases

--- TerroristGame/Animations.py
+++ TerroristGame/Animations.py
@@ -35,77 +35,83 @@ def dispararRobots(posX,posY):
     disparoRobot.disparada = True
     listaRobots.append(disparoRobot)
 
-def SpaceAtack():
-    
-    pygame.init()
-    #Tamano de la ventana
-    ventana = pygame.display.set_mode((1000,800))
-    #Nombre de la ventana
-    pygame.display.set_caption("SpaceAtack")
-    #Fondo
-    Space_imageBackground = pygame.image.load("Space/Space.jpg").convert()
-    # Definimos algunos colores
-    BLANCO = (255, 255, 255)
-    NEGRO = (0, 0, 0)
-    #Jugardor principal
-    jugador = Principal()
-    planeta1 = Planeta1()
-    planeta2 = Planeta2()
-    planeta3 = Planeta3()
-    naveRecurso = Nave()
-    sondaRecurso = Sonda()
-    robotRecurso = Robot()
-    velocidad = 8
+class SpaceAtack():
+    def __init__(self):
+        pygame.init()
+        #Tamano de la ventana
+        self.ventana = pygame.display.set_mode((1000,800))
+        #Nombre de la ventana
+        pygame.display.set_caption("SpaceAtack")
+        #Fondo
+        self.Space_imageBackground = pygame.image.load("Space/Space.jpg").convert()
+        # Definimos algunos colores
+        self.BLANCO = (255, 255, 255)
+        self.NEGRO = (0, 0, 0)
+        #Jugardor principal
+        self.jugador = Principal()
+        self.planeta1 = Planeta1()
+        self.planeta2 = Planeta2()
+        self.planeta3 = Planeta3()
+        self.naveRecurso = Nave()
+        self.sondaRecurso = Sonda()
+        self.robotRecurso = Robot()
+        self.velocidad = 8
     #Ejecucion animaciones
-    while True:
-        ventana.blit(Space_imageBackground,(0,0))
-        jugador.dibujar(ventana)
-        planeta1.dibujar(ventana)
-        planeta2.dibujar(ventana)
-        planeta3.dibujar(ventana)
-        naveRecurso.dibujar_Recurso(ventana)
-        sondaRecurso.dibujar_Recurso(ventana)
-        robotRecurso.dibujar_Recurso(ventana)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == K_LEFT:
-                    jugador.mover(jugador.get_posX()-velocidad)
-                elif event.key == K_RIGHT:
-                    jugador.mover(jugador.get_posX()+velocidad)
-                elif event.key == K_z:
-                    x,y = jugador.rect.center
-                    dispararNave(x,y)
-                elif event.key == K_x:
-                    x,y = jugador.rect.center
-                    dispararSonda(x,y)
-                elif event.key == K_c:
-                    x,y = jugador.rect.center
-                    dispararRobots(x,y)
-        if len(listaDisparos)>0:
-            for x in listaDisparos:
-                if x.disparada:
-                    x.dibujar(ventana)
-                    x.trayectoria()
-                if x.rect.top < 100:
-                    x.disparada = False
-        if len(listaSondas)>0:
-            for x in listaSondas:
-                if x.disparada:
-                    x.dibujar(ventana)
-                    x.trayectoria()
-                if x.rect.top < 100:
-                    x.disparada = False
-        if len(listaRobots)>0:
-            for x in listaRobots:
-                if x.disparada:
-                    x.dibujar(ventana)
-                    x.trayectoria()
-                if x.rect.top < 100:
-                    x.disparada = False
-#        print pygame.mouse.get_pos()
-        pygame.display.update()
 
-SpaceAtack()
+    def iniciar(self):
+        self.animacionEntradas();
+
+    def animacionEntradas(self):
+        while True:
+            self.ventana.blit(self.Space_imageBackground,(0,0))
+            self.jugador.dibujar(self.ventana)
+            self.planeta1.dibujar(self.ventana)
+            self.planeta2.dibujar(self.ventana)
+            self.planeta3.dibujar(self.ventana)
+            self.naveRecurso.dibujar_Recurso(self.ventana)
+            self.sondaRecurso.dibujar_Recurso(self.ventana)
+            self.robotRecurso.dibujar_Recurso(self.ventana)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == K_LEFT:
+                        self.jugador.mover(self.jugador.get_posX()-self.velocidad)
+                    elif event.key == K_RIGHT:
+                        self.jugador.mover(self.jugador.get_posX()+self.velocidad)
+                    elif event.key == K_z:
+                        x,y = self.jugador.rect.center
+                        dispararNave(x,y)
+                    elif event.key == K_x:
+                        x,y = self.jugador.rect.center
+                        dispararSonda(x,y)
+                    elif event.key == K_c:
+                        x,y = self.jugador.rect.center
+                        dispararRobots(x,y)
+            if len(listaDisparos)>0:
+                for x in listaDisparos:
+                    if x.disparada:
+                        x.dibujar(self.ventana)
+                        x.trayectoria()
+                    if x.rect.top < 100:
+                        x.disparada = False
+            if len(listaSondas)>0:
+                for x in listaSondas:
+                    if x.disparada:
+                        x.dibujar(self.ventana)
+                        x.trayectoria()
+                    if x.rect.top < 100:
+                        x.disparada = False
+            if len(listaRobots)>0:
+                for x in listaRobots:
+                    if x.disparada:
+                        x.dibujar(self.ventana)
+                        x.trayectoria()
+                    if x.rect.top < 100:
+                        x.disparada = False
+    #        print pygame.mouse.get_pos()
+            pygame.display.update()
+
+cliente = SpaceAtack()
+cliente.iniciar()
